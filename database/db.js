c7379089e064606ca980c706a77bacaaa365fa6e
Snehal Kale <snehal.kale@eternussolutions.com>
2018-11-20 18:57:52 +0530

    changed database config

--- database/db.js
+++ database/db.js
@@ -2,34 +2,19 @@ const sql = require('mssql')
 //const config = require('config');
 const winston = require('winston');
  
-//  var config = 
-//    {
-//      user: 'dbadmin', // update me
-//      password: 'Espl@123', // update me 
-//      server: 'einterceptor.database.windows.net', // update me
-//      database: 'EInterceptor',
-//      options: 
-//         { 
-//              encrypt: true
-//            , trustedConnection: true
-//         },
-//       sentimentApiUrl: 'https://pythonsentimentapplication.azurewebsites.net/sentiment'
-//    }; 
-
-  var config = 
+ var config = 
    {
-     user: 'sa', // update me
-     password: 'espl@123', // update me 
-     server: '192.168.101.20', // update me
+     user: 'dbadmin', // update me
+     password: 'Espl@123', // update me 
+     server: 'einterceptor.database.windows.net', // update me
      database: 'EInterceptor',
      options: 
         { 
              encrypt: true
            , trustedConnection: true
         },
-      sentimentApiUrl: 'http://localhost:5000/'
+      sentimentApiUrl: 'https://pythonsentimentapplication.azurewebsites.net/sentiment'
    }; 
-
 
 const poolPromise = new sql.ConnectionPool(config)
     .connect()
